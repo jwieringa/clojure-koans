0c58954400bbca55fb3850dc96f4811a0e32971e
Jason Wieringa <jason@jasonwieringa.com>
2013-11-29 09:44:56 -0500

    Complete 9-15 with notes on recursion

--- src/koans/13_recursion.clj
+++ src/koans/13_recursion.clj
@@ -1,20 +1,85 @@
+; Evalutes, if not nil or false, yeilds then. Otherwise yields then
 (defn is-even? [n]
   (if (= n 0)
-    __
-    (___ (is-even? (dec n)))))
+    true
+    (not (is-even? (dec n)))))
+
+; (is-even? 0) ==> (= 0 0)
+;              ==> true
+;
+; (is-even? 1) ==> (not (is-even? (dec 1))
+;              ==> (not (is-even? 0))
+;              ==> (not (= 0 0))
+;              ==> (not true)
+;              ==> false
+;
+; (is-even? 2) ==> (not (is-even? (dec 2)))
+;              ==> (not (is-even? 1))
+;              ==> (not (not is-even? (dec 1))
+;              ==> (not (not is-even? 0)
+;              ==> (not (not true)
+;              ==> (not (false)
+;              ==> (true)
+;
+; (is-even? 3) ==> (not (is-even? (dec 3))
+;              ==> (not (is-even? 2)
+;              ==> (not (not is-even? (dec 2))
+;              ==> (not (not is-even? 1))
+;              ==> (not (not (not is-even? (dec 1))))
+;              ==> (not (not (not is-even? 0)))
+;              ==> (not (not (not (= 0 0))))
+;              ==> (not (not (false)))
+;              ==> (not (true))
+;              ==> (false)
+;
+;; And taking shortcuts...
+;
+; (is-even? 4) ==> (not (is-even? 3)
+;              ==> (not (not is-even? 2))
+;              ==> (not (not (not is-even? 1)))
+;              ==> (not (not (not (not is-even? 0))))
+;              ==> (not (not (not (not (= 0 0)))))
+;              ==> (not (not (not (not (true)))))
+;              ==> (not (not (not (false))))
+;              ==> (not (not (true)))
+;              ==> (not (false))
+;              ==> (true)
 
 (defn is-even-bigint? [n]
-  (loop [n   n
+  (loop [n n
          acc true]
     (if (= n 0)
-      __
+      acc
       (recur (dec n) (not acc)))))
 
 (defn recursive-reverse [coll]
-  __)
+  (loop [coll coll
+         reversed ()]
+    (if (empty? coll)
+      reversed
+      (recur (rest coll) (cons (first coll) reversed)))))
 
 (defn factorial [n]
-  __)
+  (loop [n n
+         acc 1]
+    (if (= 0 n)
+      acc
+      (recur (dec n) (* n acc)))))
+
+; Since solving the fib sequence is finite in these meditaitons,
+; using recur is probably a good solution. However, if it were
+; a infinite sequence, a lazy sequence should be used. fib-step
+; and fib-seq use lazy sequences to solve for a finite fib, but
+; does demonstrate the pattern.
+;
+; (defn fib-step [[a b]]
+;     [b (+ a b)])
+;
+; (defn fib-seq []
+;     ; key to this pattern is map and iterate.
+;     ; both return a lazy sequence meaning that they are only
+;     ; evaluated when necessary instead of upon a function call
+;     (map first (iterate fib-step [0 1])))
 
 (meditations
   "Recursion ends with a base case"
